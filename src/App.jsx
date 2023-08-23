af4ec73f0ae7c000147fc9a59ee87c673067a46c
Alok <alokdebnath.in@gmail.com>
2023-08-23 15:21:33 +0530

    removed previously added experimental stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import "./App.scss";
 
 
 import Index from './Components/Pages/Index';
-// import Todos from './Components/Pages/Todos';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} exact />
-        {/* <Route path="/todo" element={<Todos />} exact /> */}
       </Routes>
     </BrowserRouter>
   );
